d2706386f064f28bb1f938a56616f73784f1dc42
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-08-05 22:45:30 -0400

    Converted toSpeak function into an arrow function expression.

--- assignments/objects.js
+++ assignments/objects.js
@@ -136,8 +136,5 @@ console.log(parent.child.grandchild.speak());
 
 // ANOTHER SOLUTION FOR SPEAK METHOD
 // Reduces Repetative Code
-const toSpeak = function(a) {
-  return `Hello, my name is ${a.name}.`;
-};
-
+const toSpeak = a => `Hello, my name is ${a.name}.`;
 console.log(toSpeak(parent));
